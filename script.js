3b5b71e1f57f36b4ca7ed19db038538311392efa
gustavo <gustavo@gpsbr.net>
2023-11-28 17:25:49 -0300

    commitando a questão 37

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ alert("Total a pagar:"+ total)*/
 
 /*Crie um programa que leia duas notas de um aluno e calcule a sua média, mostrando uma mensagem no final, de acordo com a média atingida: -média até 4,9: reprovado -média entre 5.0 e 6.9: recuperação -media 7 ou superior: aprovado */
 
-let n1 = prompt("informe a primeira nota");
+/*let n1 = prompt("informe a primeira nota");
 let n2 = prompt("informe a segunda nota");
 
 let média;
@@ -73,6 +73,89 @@ if (média <= 4.9){
 }else{
     alert("Aprovado");
 }
+*/
+
+/*let nome = prompt ("Digite seu nome:")
+let sal = prompt ("Digite seu salário:")
+let temp = prompt ("Digite há quantos anos você trabalha na empresa:")
+let conv, salf;
+
+
+if(temp <=3){
+    conv = Number(sal) *0.03;
+}
+else if (temp >= 4 && temp <= 10){
+    conv = Number(sal) * 0.125;
+}
+else{
+    conv = Number(sal) * 0.2;
+}
+    salf= Number(sal) + Number(sal);
+
+    alert(nome + "seu salário teve um aumento de: " + conv + "R$" +"e seu novo salário é de:" + "R$");*/
+
+    /*let Larg = prompt ("Digite a Largura");
+    let comp = prompt ("Digite o Comprimento");
+    let area;
+
+    area = (Number (Larg) * Number (comp));
+
+    if (area <=100){
+        alert ("Terreno Popular");
+    }
+    else if (area >100 && area <=500){
+        alert ("Terreno Master");
+    }
+    else {
+        alert ("Terreno VIP");
+    }*/
+
+    /*let horas = prompt ("Digite quantas horas")
+    let pont;
+    let dinheiro;
+
+    if (horas <=10){
+        pont= Number(horas) *2
+    }
+
+    else if (horas >10 && horas <=20){
+        pont = Number(horas) *5
+    }    
+    else {
+        pont = Number(horas) *10
+    }
+       dinheiro = pont * 0.05
+       alert ("ganho total: " + dinheiro)*/
+
+       let sal = (prompt ("Digite o salário"));
+       let gen = (prompt ("Digite o gênero"));
+       let temp= (prompt ("Digite tempo de trabalho")); 
+       let novosalario; 
+       
+       if(gen == "m"){
+           if (Number(temp <20))
+           novosalario = Number (sal) * 0.03;
+        }else if (temp =20 && temp <=30){
+           novosalario = Number (sal) * 0.13;
+        } else {
+           novosalario = Number(sal) * 0.25;
+        }
+       if (gen == "f"){
+           if (Number(temp <15))
+           novosalario = Number(sal) * 0.05;
+       }else if (temp =15 && temp <=20){
+           novosalario = Number(sal) * 0.12;
+       }else {
+           novosalario = Number(sal) * 0.23;
+       }
+
+       alert ("Novo salário é: " + novosalario)
+            
+        
+
+
+
+
 
 
 
